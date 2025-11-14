d2838f83d04196acd652b04efbb4aab8a45c79c9
kynhix <kristopher.t.hicks@gmail.com>
2025-11-13 21:53:27 -0500

    Update UI

--- dashboard/components/AppSidebar.tsx
+++ dashboard/components/AppSidebar.tsx
@@ -1,59 +1,101 @@
 "use client"
 import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupLabel, SidebarHeader, SidebarInput, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar"
-import { useState } from "react";
+import { ChartLine, ChevronDown, ChevronUp, Frown, Minus } from "lucide-react";
+import { useMemo, useState } from "react";
 
-const items = [
+type Item = {
+  title: string,
+  url: string,
+  price: number,
+  change: number,
+}
+
+const items: Item[] = [
   {
     title: "AAPL",
     url: "#",
+    price: 155.09,
+    change: 1.1,
   },
   {
     title: "TSLA",
     url: "#",
+    price: 155.23,
+    change: 5.1,
   },
   {
     title: "NVDA",
     url: "#",
+    price: 1238,
+    change: -1.1,
   },
   {
     title: "AMD",
     url: "#",
+    price: 15,
+    change: 0,
   },
   {
     title: "MSFT",
     url: "#",
+    price: 128,
+    change: -5.1,
   },
 ]
+
+function getIconAndColor(change: number) {
+  if (change > 0) {
+    return { icon: ChevronUp, color: '#00a500' };
+  } else if (change < 0) {
+    return { icon: ChevronDown, color: '#a50000' };
+  } else {
+    return { icon: Minus, color: '#555' };
+  }
+}
 
 export function AppSidebar() {
   const [searchTerm, setSearchTerm] = useState('');
 
+  const searchItems = useMemo(() =>
+  (items
+    .filter((item) =>
+      item.title.toLowerCase().includes(searchTerm.toLowerCase())
+    ).map((item) => ({ ...item, ...getIconAndColor(item.change) })))
+    , [searchTerm]);
+
   return (
     <Sidebar>
-      <SidebarHeader>Stock Dashboard</SidebarHeader>
+      <SidebarHeader className="flex flex-col gap-5">
+        <div className="flex gap-2">
+          <ChartLine />
+          <h1>Stock Dashboard</h1>
+        </div>
+        <SidebarInput
+          id="search"
+          placeholder="Search for stock..."
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </SidebarHeader>
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>Stocks</SidebarGroupLabel>
-          <SidebarInput
-            type="search"
-            placeholder="Search for stock..."
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
           <SidebarMenu>
-            {items
-              .filter((item) =>
-                searchTerm === '' ||
-                item.title.toLowerCase().includes(searchTerm.toLowerCase())
-              )
-              .map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
+            {
+              searchItems.length > 0 ? searchItems
+                .map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <a href={item.url}>
+                        <item.icon className="opacity-80" color={item.color} />
+                        <span className="w-11">{item.title}</span>
+                        <span className="opacity-50">|</span>
+                        <span className="opacity-80 pl-1.5">${item.price.toFixed(2)}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))
+                : <div className="flex ml-auto mr-auto gap-2 opacity-80">No stocks found <Frown /></div>
+            }
           </SidebarMenu>
         </SidebarGroup>
       </SidebarContent>
