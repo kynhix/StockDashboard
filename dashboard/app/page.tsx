553b791cfc1cb0476d7e74d342ba838779223a15
kynhix <kristopher.t.hicks@gmail.com>
2025-11-19 21:38:26 -0500

    Add welcome text and fix layout

--- dashboard/app/page.tsx
+++ dashboard/app/page.tsx
@@ -1,10 +1,11 @@
-import { AppSidebar } from "@/components/AppSidebar";
-import { SidebarProvider } from "@/components/ui/sidebar";
 
 export default function Home() {
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-    </SidebarProvider>
+  return (<main className="flex-1">
+    <div className="flex flex-col gap-4 p-4 max-w-200">
+      <div className="flex-1 basis-0 text-2xl">Welcome to the stock dashboard!</div>
+      <div className="flex-1 basis-0 text-lg opacity-90">View stocks, make predictions, and get rich.</div>
+      <div className="flex-1 basis-0 text-lg opacity-90">Select a stock to get started.</div>
+    </div>
+  </main>
   );
 }
