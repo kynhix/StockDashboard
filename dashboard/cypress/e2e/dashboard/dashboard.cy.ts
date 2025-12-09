26224cc7caa25a6994d91cf2a598a00865b49265
kynhix <kristopher.t.hicks@gmail.com>
2025-12-08 21:45:31 -0500

    Add tests

--- dashboard/cypress/e2e/dashboard/dashboard.cy.ts
+++ dashboard/cypress/e2e/dashboard/dashboard.cy.ts
@@ -5,8 +5,33 @@ describe('example to-do app', () => {
     cy.intercept('GET', 'http://localhost:8000/predict/*', { fixture: 'predict.json' }).as('getPredict')
   })
 
-  it('Should have welcome text', () => {
+  it('Validate page layout', () => {
     cy.visit('/')
-    cy.get('header').first().should('have.text', 'Welcome to the Stock Dashboard')
+
+    cy.prompt([
+      'Make sure there is welcome text on the page',
+      "Verify that there is a sidebar with different stocks listed and their prices",
+      'Verify there is a searchbar located in the sidebar to search for stocks'
+    ])
+  })
+
+  it('Verify that the search bar works', () => {
+    cy.visit('/')
+
+    cy.prompt([
+      'Make sure a searchbar exists on the sidebar',
+      "Type in the word 'Apple' into the searchbar and verify that the 'AAPL' stock is visible",
+      'Click on the APPL stock from the sidebar and verify it takes you to the \'AAPL\' stock page'
+    ])
+  })
+
+  it('Verify making a prediction works', () => {
+    cy.visit('/')
+
+    cy.prompt([
+      'Click on the AAPL stock in the sidebar',
+      "Verify there are different stats about the stock such as change, high, close, open",
+      'Click the Predict button and verify that a green block appears with the text Buy'
+    ])
   })
 })
